8c2348b2b1656c6e01a3e7f1c944e4ae9205f1ff
PauloCamargos <paulo.camargos@ufu.br>
2018-06-03 22:26:32 -0300

    fix bug in .ino

--- printSL/printSL.ino
+++ printSL/printSL.ino
@@ -15,48 +15,58 @@
    3 - Verificar porta serial. Se disponivel, armazenar em um vetor o valor retornado
    4 - Enviar o vetor para o programa em Python.
 */
+#include "SoftwareSerial.h"
+SoftwareSerial xbee_serial(2, 3); //RX, TX
 
 uint8_t option;
 int response[4] = {};
 
-int sl_frames[3][19] = { // Array de frames
+byte sl_frames[][19] = { // Array de frames
   {0x7E, 0x00, 0x0F, 0x17, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x7C, 0x48, 0xFF, 0xFF, 0xFE, 0x02, 0x53, 0x4C, 0x91}, //E1
   {0x7E, 0x00, 0x0F, 0x17, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x7C, 0x49, 0x27, 0xFF, 0xFE, 0x02, 0x53, 0x4C, 0x68}, // E2
   {0x7E, 0x00, 0x0F, 0x17, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x7C, 0x48, 0xFE, 0xFF, 0xFE, 0x02, 0x53, 0x4C, 0x92}  //E3
 };
 
+
+
 void setup() {
   Serial.begin(9600);
+  xbee_serial.begin(9600);
 }
 
-void send_frame(int frame[19]) {
-  for (int i = 0; i < sizeof(frame) / sizeof(int); i++) {
-    Serial.write((int) frame[i]);
+void send_frame(byte frame[19]) {
+  Serial.println("Enviando frame...");
+  for (int i = 0; i < 19; i++) {
+    Serial.print((byte)frame[i], HEX);
+    xbee_serial.write((byte)frame[i]);
   }
 }
 
 void loop() {
+
   if (Serial.available() == 1) {
     option = Serial.read();
     switch (option) {
-      case 1: // Retorna o SL do 1
+      case '1': // envia frame para o ED 1
         send_frame(sl_frames[0]);
         break;
-      case 2: // Retorna o SL do 2
+      case '2': // envia frame para o ED 2
         send_frame(sl_frames[1]);
         break;
-      case 3: // Retorna o SL do 2
+      case '3': // envia frame para o ED 3
         send_frame(sl_frames[2]);
         break;
     }
-  } else if (Serial.available() > 21) {
-    if (Serial.read() == 0x7E) { // se for o inicio do pacote
-      for (int i = 0; i < 17; i++) { //loop sobre a resposta
-        uint8_t waste = Serial.read();
+  }
+  if (xbee_serial.available() > 21) {
+    if (xbee_serial.read() == 0x7E) { // se for o inicio do pacote
+      for (int i = 0; i < 17; i++) { // loop sobre a resposta
+        uint8_t waste = xbee_serial.read(); // lixo 
       }
-      for (int i = 0; i < 4; i++) { // loop para armazenar o SL recebido
-        response[i] = Serial.read();
-        Serial.print(response[i]);
+      for (int i = 0; i < 4; i++) { // loop para armazenar o SL do pacote recebido
+        response[i] = xbee_serial.read();
+        Serial.print(response[i], HEX);
+        Serial.print('-');
       }
     }
     Serial.println();
